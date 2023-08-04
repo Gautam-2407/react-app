80287ae7e1cb4b044333b090fc47e9e67366a0c8
mavmodz <maverickjetman07@gmail.com>
2023-08-04 13:52:23 +0530

    changes on login and register

--- frontend/src/page/Login/Login.jsx
+++ frontend/src/page/Login/Login.jsx
@@ -1,28 +1,27 @@
-import React from 'react'
-import './Login.css'
-import { Link } from 'react-router-dom'
+import React from "react";
+import "./Login.css";
+import { Link } from "react-router-dom";
 
 function Login() {
   return (
-    <div className="container">
-
-  <div className="login-form">
-   <h1>Login</h1>
-   <form>
-   <input type="text" name="username" placeholder="Username" />
-   <input type="password" name="password" placeholder="Password" />
-   <div className="password">
-    <Link to="/">Forget Password</Link>
-   </div>
-   <input type="submit" value="Login" />
-   <div className="singin">
-    Not a Member?
-    <Link to="/register">Sign In</Link>
-   </div>
-   </form>
-   </div>
+    <div className="container-login">
+      <div className="login-form">
+        <h1>Login</h1>
+        <form>
+          <input type="text" name="username" placeholder="Username" />
+          <input type="password" name="password" placeholder="Password" />
+          <div className="password">
+            <Link to="/">Forget Password</Link>
+          </div>
+          <input type="submit" value="Login" />
+          <div className="singin">
+            Not a Member?
+            <Link to="/register">Sign In</Link>
+          </div>
+        </form>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Login
+export default Login;
--- frontend/src/page/Register/Register.jsx
+++ frontend/src/page/Register/Register.jsx
@@ -1,38 +1,60 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import "./register.css"
+import React from "react";
+import { Link } from "react-router-dom";
+import "./register.css";
 
 function Register() {
   return (
-    <><div class="container">
-      <h2>Registration Form</h2>
-      <form>
-        <div class="form-group">
-          
-          <input type="text" id="username" name="username"  placeholder='username'required  />
+    <div className="main-register">
+      <div class="container-register">
+        <h2>Registration Form</h2>
+        <form>
+          <div class="form-group">
+            <input
+              type="text"
+              id="username"
+              name="username"
+              placeholder="username"
+              required
+            />
           </div>
-        <div class="form-group">
-          
-          <input type="email" id="email" name="email"  placeholder='email'required />
+          <div class="form-group">
+            <input
+              type="email"
+              id="email"
+              name="email"
+              placeholder="email"
+              required
+            />
           </div>
-        <div class="form-group">
-          
-          <input type="password" id="password" name="password" placeholder='password' required />
-        </div>
-        <div class="form-group">
-          
-          <input type="password" id="confirm-password" name="confirm-password"  placeholder='confirm-password'required />
+          <div class="form-group">
+            <input
+              type="password"
+              id="password"
+              name="password"
+              placeholder="password"
+              required
+            />
           </div>
-        <div class="form-group">
-          <input type="submit" value="Register" />
+          <div class="form-group">
+            <input
+              type="password"
+              id="confirm-password"
+              name="confirm-password"
+              placeholder="confirm-password"
+              required
+            />
           </div>
-      </form>
-    </div><div className="login2">
+          <div class="form-group">
+            <input type="submit" value="Register" />
+          </div>
+        </form>
+      </div>
+      <div className="login2">
         Already Menber
         <Link to="/">Login</Link>
-      </div></>
-
-  )
+      </div>
+    </div>
+  );
 }
 
-export default Register
+export default Register;
