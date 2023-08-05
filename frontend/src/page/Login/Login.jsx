4a753c58e1ddd5b8c199439e5a60661122965ad0
mavmodz <maverickjetman07@gmail.com>
2023-08-05 09:48:22 +0530

    staging changes

--- frontend/src/page/Login/Login.jsx
+++ frontend/src/page/Login/Login.jsx
@@ -4,21 +4,23 @@ import { Link } from "react-router-dom";
 
 function Login() {
   return (
-    <div className="container-login">
-      <div className="login-form">
-        <h1>Login</h1>
-        <form>
-          <input type="text" name="username" placeholder="Username" />
-          <input type="password" name="password" placeholder="Password" />
-          <div className="password">
-            <Link to="/">Forget Password</Link>
-          </div>
-          <input type="submit" value="Login" />
-          <div className="singin">
-            Not a Member?
-            <Link to="/register">Sign In</Link>
-          </div>
-        </form>
+    <div className="main-login">
+      <div className="container-login">
+        <div className="login-form">
+          <h1>Login</h1>
+          <form>
+            <input type="text" name="username" placeholder="Username" />
+            <input type="password" name="password" placeholder="Password" />
+            <div className="password">
+              <Link to="/">Forget Password</Link>
+            </div>
+            <input type="submit" value="Login" />
+            <div className="singin">
+              Not a Member?
+              <Link to="/register">Sign In</Link>
+            </div>
+          </form>
+        </div>
       </div>
     </div>
   );
