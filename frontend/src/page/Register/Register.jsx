46be5026fd1cca608b5c3cb8afb78a6391a19b48
Gautam-2407 <gautambhatia2407@gmail.com>
2023-08-11 15:11:36 +0530

    register page

--- frontend/src/page/Register/Register.jsx
+++ frontend/src/page/Register/Register.jsx
@@ -1,64 +1,66 @@
-import React, {useState} from "react";
-import {  Link, useNavigate } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import "./register.css";
-import {registerfunction} from "../../services/apis"
+import { registerfunction } from "../../services/apis"
 
 function Register() {
-  const [username, setUsername]=useState("");
-  const [email, setEmail]=useState("");
-  const [password, setPassword]=useState("");
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const navigate = useNavigate();
 
-  const handleClick= async (e)=>{
+  const handleClick = async (e) => {
     e.preventDefault();
     console.log("Register button clicked");
-    try{
+    try {
       console.log("Register");
-     const response = await registerfunction(username, email, password);
+      const response = await registerfunction(username, email, password);
 
       console.log(response);
       navigate("/")
-   }
-    catch(error){
+    }
+    catch (error) {
       console.log(error);
     }
   }
   return (
     <div className="main-register">
-      <div class="container-register">
-        <h2>Registration Form</h2>
-        <form className="register-form-wrapper">
-          <div class="form-group">
-            <input type="text" id="username" name="username" placeholder="username" required value={username} onChange={(e) => setUsername(e.target.value)}/>
+      <div className="container-border">
+        <div class="container-register">
+          <h2>Registration Form</h2>
+          <form className="register-form-wrapper">
+            <div class="form-group">
+              <input type="text" id="username" name="username" placeholder="username" required value={username} onChange={(e) => setUsername(e.target.value)} />
+            </div>
+            <div class="form-group">
+              <input
+                type="email"
+                id="email"
+                name="email"
+                placeholder="email"
+                required
+                value={email} onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+            <div class="form-group">
+              <input
+                type="password"
+                id="password"
+                name="password"
+                placeholder="password"
+                required
+                value={password} onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+            <div class="form-group">
+              <input type="submit" value="Register" onClick={handleClick} />
+            </div>
+          </form>
+          <div className="login2">
+            Already Menber
+            <Link to="/">Login</Link>
           </div>
-          <div class="form-group">
-            <input
-              type="email"
-              id="email"
-              name="email"
-              placeholder="email"
-              required
-              value={email} onChange={(e) => setEmail(e.target.value)}
-            />
-          </div>
-          <div class="form-group">
-            <input
-              type="password"
-              id="password"
-              name="password"
-              placeholder="password"
-              required
-              value={password} onChange={(e) => setPassword(e.target.value)}
-            />
-          </div>
-          <div class="form-group">
-            <input type="submit" value="Register" onClick={handleClick} />
-          </div>
-        </form>
-      <div className="login2">
-        Already Menber
-        <Link to="/">Login</Link>
-      </div>
+        </div>
       </div>
     </div>
 
@@ -79,7 +81,7 @@ function Register() {
     //             onChange={(e) => setUsername(e.target.value)}
     //             placeholder="Enter Name"
     //           />
-             
+
     //         </div>
     //         <div className="text-field">
     //           <label htmlFor="email">Email Address</label>
@@ -92,7 +94,7 @@ function Register() {
     //             placeholder="Enter Email"
     //             required
     //           />
-              
+
     //         </div>
     //         <div className="text-field">
     //           <label htmlFor="password">Phone Number</label>
@@ -103,10 +105,10 @@ function Register() {
     //             value={password}
     //             onChange={(e) => setPassword(e.target.value)}
     //             placeholder="Enter Phone Number"
-                
+
     //             required
     //           />
-              
+
     //         </div>
     //         <div className="Get_Otp">
     //           <input
@@ -116,7 +118,7 @@ function Register() {
     //             onClick={handleClick}
     //           />
     //         </div>
-            
+
     //       </form>
     //     </span>
     //   </div>
