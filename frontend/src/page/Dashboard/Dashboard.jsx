38c323dcd134a6c4009fb1f89e268bee00d7bf6e
Gautam-2407 <gautambhatia2407@gmail.com>
2023-08-12 17:29:09 +0530

    sidebar changes

--- frontend/src/page/Dashboard/Dashboard.jsx
+++ frontend/src/page/Dashboard/Dashboard.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Header from "../../components/header/Header";
-import Sidebar from '../../components/Sidebar/Sidebar';
+
 import Drawer from '../../components/drawer/Drawer';
 
 function Dashboard() {
