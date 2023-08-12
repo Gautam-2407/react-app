2e80f6706ca5d484fde750cf30ebdf37c65f0c75
Ramanwork24 <sainiraman8700@gmail.com>
2023-08-12 15:36:32 -0700

    side bar completed

--- frontend/src/components/Sidebar/Sidebar.jsx
+++ frontend/src/components/Sidebar/Sidebar.jsx
@@ -1,26 +1,48 @@
-import React from 'react'
+import React from 'react';
+import "./Sidebar.css";
+import logo from "../assets/images/logo.png";
+import { LayoutDashboard, User, Wallet, LogOut } from "lucide-react";
+import { Link } from 'react-router-dom';
+const Sidebar = () => {
+    return (
 
-const Sidebar=()=>{
-  return (
-    
-        <div classname="sidebar-box">
-            <div classname="sidebar-logo">
-                <a href="index.html"><img src="images/logo.png" alt="" /></a>
+        <div className="sidebar-container">
+            <div className="top">
+                <img src={logo} alt="logo" />
             </div>
-            <div classname="sidebar-menu">
-                <ul>
-                    <li><a href="index.html">Home</a></li>
-                    <li><a href="about.html">About</a></li>
-                    <li><a href="services.html">Services</a></li>
-                    <li><a href="portfolio.html">Portfolio</a></li>
-                    <li><a href="blog.html">Blog</a></li>
-                    <li><a href="contact.html">Contact</a></li>
-                </ul>
+            <div className="sidebar-menu">
+                {/* Dashboard link Open */}
+                <div className="sidebar-wrapper">
+                    <LayoutDashboard color="#3574F2" strokeWidth={1.5} />
+                    <Link to={"/dashboard"} className='page-links'>Dashboard</Link>
+                </div>
+                {/* Dashboard link Closed */}
 
+                {/* User link Open */}
+                <div className="sidebar-wrapper">
+                    <User color="#3574F2" strokeWidth={1.5} />
+                    <Link to={"/register"} className='page-links'>Profile</Link>
+                </div>
+                {/* User link Closed */}
+               
+               {/* Checkout link is open */}
+                <div className="sidebar-wrapper">
+                    <Wallet color="#3574F2" strokeWidth={1.5} />
+                    <Link to={"/checkout"} className='page-links' >Checkout</Link>
+                </div>
+                {/* Checkout link is closed */}
+
+                {/* Logout link is open */}
+                <div className="sidebar-wrapper">
+                    <LogOut color="#3574F2" strokeWidth={1.5} />
+                    <Link to={"/logout"} className='page-links' >Logout</Link>
+                </div>
+                {/* Logout link is closed */}
+
+            </div>
         </div>
-      
-    </div>
-  )
+
+    );
 }
 
 export default Sidebar;
