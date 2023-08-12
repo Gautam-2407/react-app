d5caad16fc23e8ce9f87611090567251987d97df
Ramanwork24 <sainiraman8700@gmail.com>
2023-08-12 11:28:48 -0700

    master

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,18 @@
 import React from "react";
-import {Routes, Route } from "react-router-dom";
-import Register from "./page/Register/Register";
-import Login from "./page/Login/Login";
+// import {Routes, Route } from "react-router-dom";
+// import Register from "./page/Register/Register";
+// import Login from "./page/Login/Login";
+import Header from "./components/header/Header";
 import './App.css';
 
 
 function App() {
   return (
+    
 
     <div className="App">
-      <Routes>
-        <Route path= '/' Component={Login} />
-        <Route path= '/register' Component={Register} />
-      </Routes>
+      <Header />
+             
 
     
     </div>
--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -1,11 +1,29 @@
 import React from 'react';
 import './header.css'
-function Header() {
-    return(
+import { Menu, BellRing } from "lucide-react";
+import userimage from '../assets/images/user-2.jpg';
 
-    <div>Header</div>
+const Header = () => {
+    return (
 
+        <div className="main-container">
+            <div className="header-container">
+                <div className="header-left">
+                    <div className="logo">
+                        <Menu color="#3574F2" strokeWidth={1.5} />
+                    </div>
+                </div>
+                <div className="header-right">
+                    <div className="header-bell">
+                        <BellRing size={24} />
+                    </div>
+                    <div className="header-user">
+                        <img src={userimage} alt="user" />
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
-export default Header
+export default Header;
