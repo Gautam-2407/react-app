8fba68c1bdeeaee6bdc694d40d91bc1feb1fc549
Gautam-2407 <gautambhatia2407@gmail.com>
2023-08-14 16:13:26 +0530

    sidebar hide or unhide

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -3,6 +3,16 @@ import './header.css'
 import { Menu, BellRing } from "lucide-react";
 import userimage from '../assets/images/user-2.jpg';
 
+
+const close = (()=>{
+  var x = document.getElementById("sidebar");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+})
+
 const Header = () => {
     return (
 
